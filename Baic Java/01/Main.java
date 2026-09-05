92593f1420dd2ab56a64d6d76294824cac3ea3e1
Varun Mangwani <vpm.media.business@gmail.com>
2026-09-05 18:34:06 +0000

    Implement main method for student marks input and calculation

--- Baic Java/01/Main.java
+++ Baic Java/01/Main.java
@@ -1,5 +1,26 @@
 import java.util.*;
 
 public class Main{
-    
-}
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter The Name: ");
+        String Name = scan.nextLine();
+        int[] Marks = new int[5];
+        System.out.print("Enter The Physics Marks: ");
+        Marks[0] = scan.nextInt();
+        System.out.print("Enter The Chemistry Marks: ");
+        Marks[1] = scan.nextInt();
+        System.out.print("Enter The Maths Marks: ");
+        Marks[2] = scan.nextInt();
+        System.out.print("Enter The English Marks: ");
+        Marks[3] = scan.nextInt();
+        System.out.print("Enter The Computer Marks: ");
+        Marks[4] = scan.nextInt();
+        int Total = Marks[0] + Marks[1] + Marks[2] + Marks[3] + Marks[4];
+        System.out.println("Total Marks Obtained Is" + Total);
+        System.out.println("Percentage Is " + (Total / 5) + "%");
+
+        
+        scan.close();
+    }
+ }
