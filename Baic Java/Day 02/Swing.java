a38b2c356605e533512d87a638db80318d548c31
Varun Mangwani <vpm.media.business@gmail.com>
2026-09-06 10:05:39 +0000

    Remove Main.java and Main.class files; reintroduce Main.java with student marks input and calculation logic

--- Baic Java/Day 02/Swing.java
+++ Baic Java/Day 02/Swing.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
 
+//Class Swing 
 public class Swing{
+    //Entry Point Main
     public static void main(String[] args) {
+        //JFrame Obect Making By Name "frame"
         JFrame frame = new JFrame();
+        //Setting Visiblity
         frame.setVisible(true);
+        //Setting Size 700px By 700Px
         frame.setSize(700, 700);
+        //Title For App
         frame.setTitle("Coding Is My Love And I Felled In Love With Coding");
+        //Default Operation For App Window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
